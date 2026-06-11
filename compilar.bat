57a0d79682f007ee9d4761c7ae7e6df2de829f14
edmarsanchez0618 <edmar.sanchez@ingenieria.unam.edu>
2026-06-11 14:23:25 -0600

    Update compilar.bat

--- compilar.bat
+++ compilar.bat
@@ -1,13 +1,13 @@
 @echo off
 echo [INFO] Compilando el Ensamblador Modular en C...
-gcc -Isrc src/main.c src/lexer.c src/parser.c src/encoder.c src/linker.c -o ensamblador.exe
+gcc -Iinclude src/main.c src/lexer.c src/parser.c src/encoder.c src/linker.c -o ensamblador.exe
 
 if %errorlevel% equ 0 (
     echo [OK] Ensamblador compilado con exito.
     echo ---------------------------------------------------
     echo [INFO] Compilando la Suite de Pruebas en C...
     gcc tests/test_runner.c -o tests/test_runner.exe
-    
+
     if %errorlevel% equ 0 (
         echo [OK] Suite de pruebas lista. Iniciando ejecucion...
         echo ===================================================
