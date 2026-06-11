0aedacd0d8459553f853a22ce90e25b7cf0c90d4
edmarsanchez0618 <edmar.sanchez@ingenieria.unam.edu>
2026-06-10 23:45:08 -0600

    Add batch file for compiling assembler and tests

---
+++ compilar.bat
@@ -0,0 +1,21 @@
+@echo off
+echo [INFO] Compilando el Ensamblador Modular en C...
+gcc -Isrc src/main.c src/lexer.c src/parser.c src/encoder.c src/linker.c -o ensamblador.exe
+
+if %errorlevel% equ 0 (
+    echo [OK] Ensamblador compilado con exito.
+    echo ---------------------------------------------------
+    echo [INFO] Compilando la Suite de Pruebas en C...
+    gcc tests/test_runner.c -o tests/test_runner.exe
+    
+    if %errorlevel% equ 0 (
+        echo [OK] Suite de pruebas lista. Iniciando ejecucion...
+        echo ===================================================
+        .\tests\test_runner.exe
+    ) else (
+        echo [ERROR] No se pudo compilar el archivo de pruebas tests/test_runner.c
+    )
+) else (
+    echo [ERROR] Hubo un problema al compilar el codigo fuente del ensamblador.
+)
+pause
