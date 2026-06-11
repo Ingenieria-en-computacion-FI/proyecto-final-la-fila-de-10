c7b9cddcc961b11b99d8c4fea31c0ae38a2902d8
edmarsanchez0618 <edmar.sanchez@ingenieria.unam.edu>
2026-06-11 13:56:34 -0600

    Update linker.c

--- src/linker.c
+++ src/linker.c
@@ -7,11 +7,10 @@ extern int indice_binario;
 extern Symbol tabla_simbolos[];
 extern int contador_simbolos;
 
-// Estructura clásica de un archivo objeto simple
 typedef struct {
-    char firma[4];          // "OBJ1"
-    int tamano_texto;       // Tamaño del código máquina
-    int num_simbolos;       // Cantidad de etiquetas exportadas
+    char firma[4];          
+    int tamano_texto;       
+    int num_simbolos;       
 } CabeceraObjeto;
 
 void generar_archivo_objeto(const char *nombre_archivo) {
@@ -21,16 +20,38 @@ void generar_archivo_objeto(const char *nombre_archivo) {
         return;
     }
 
-    // 1. Escribir Cabecera
     CabeceraObjeto cabecera = { {'O', 'B', 'J', '1'}, indice_binario, contador_simbolos };
     fwrite(&cabecera, sizeof(CabeceraObjeto), 1, out);
-
-    // 2. Escribir la sección de código máquina (.text)
     fwrite(buffer_binario, sizeof(uint8_t), indice_binario, out);
-
-    // 3. Escribir Tabla de Símbolos para que el Linker final la resuelva
     fwrite(tabla_simbolos, sizeof(Symbol), contador_simbolos, out);
 
     fclose(out);
     printf("[EXITO] Archivo objeto '%s' generado correctamente (%d bytes de texto).\n", nombre_archivo, indice_binario);
+}
+
+void generar_binario_final(const char *archivo_obj, const char *archivo_bin) {
+    FILE *in = fopen(archivo_obj, "rb");
+    FILE *out = fopen(archivo_bin, "wb");
+    
+    if (!in || !out) {
+        printf("[ERROR DEL LINKER] Fallo al manipular archivos finales.\n");
+        if (in) fclose(in);
+        if (out) fclose(out);
+        return;
+    }
+
+    CabeceraObjeto cabecera;
+    fread(&cabecera, sizeof(CabeceraObjeto), 1, in);
+
+    uint8_t temp_text[2048];
+    fread(temp_text, sizeof(uint8_t), cabecera.tamano_texto, in);
+
+    Symbol temp_simbolos[100];
+    fread(temp_simbolos, sizeof(Symbol), cabecera.num_simbolos, in);
+
+    fwrite(temp_text, sizeof(uint8_t), cabecera.tamano_texto, out);
+
+    fclose(in);
+    fclose(out);
+    printf("[EXITO] Linker completado. Binario '%s' generado.\n", archivo_bin);
 }
